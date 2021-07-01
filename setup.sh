302224ad631fce5c3323e6d4222ea8a61dbca688
kB01guy <kb01.cmd@protonmail.com>
2021-07-01 16:58:14 +0200

    - Update setup Script - include more Plugin Configs

--- setup.sh
+++ setup.sh
@@ -2,8 +2,6 @@
 
 BASEDIR=$(pwd)
 
-MANUAL_DOWNLOAD="\nPlease Download these Plugins manually! \n\n"
-
 # Helper
 get_latest_release() {  # Source: https://gist.github.com/lukechilds/a83e1d7127b78fef38c2914c4ececc3c
   curl --silent "https://api.github.com/repos/$1/releases/latest" | # Get latest release from GitHub api
@@ -55,6 +53,8 @@ cd ${BASEDIR}/server && cp ${BASEDIR}/tools/buildtools/spigot*.jar ./spigot.jar
   # Download Plugins
   cd ${BASEDIR}/server/plugins
 
+  MANUAL_DOWNLOAD="\nPlease Download these Plugins manually to use the latest Version! \n\n"
+
   # BackupOnEvent https://www.curseforge.com/minecraft/bukkit-plugins/backuponevent
   LATEST=$(get_latest_release enayet123/BackupOnEventPlugin)
   curl -OL https://github.com/enayet123/BackupOnEventPlugin/releases/download/${LATEST}/BackupOnEvent.jar
@@ -67,10 +67,13 @@ cd ${BASEDIR}/server && cp ${BASEDIR}/tools/buildtools/spigot*.jar ./spigot.jar
   curl -OL https://github.com/EssentialsX/Essentials/releases/download/${LATEST}/EssentialsXChat-${LATEST}.0.jar
   # GriefPrevention https://www.curseforge.com/minecraft/bukkit-plugins/grief-prevention
   MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD}GriefPrevention: https://www.curseforge.com/minecraft/bukkit-plugins/grief-prevention \n"
+  curl -OL https://media.forgecdn.net/files/3173/411/GriefPrevention.jar
   # ArmorStandEditor https://www.curseforge.com/minecraft/bukkit-plugins/armor-stand-edit
   MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD}ArmorStandEditor: https://www.curseforge.com/minecraft/bukkit-plugins/armor-stand-edit \n"
+  curl -OL https://media.forgecdn.net/files/2999/757/armorstandeditor-1.16-25.jar
   # LuckPerms https://github.com/lucko/LuckPerms
   MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD}LuckPerms: https://ci.lucko.me/job/LuckPerms/lastSuccessfulBuild/artifact/bukkit/loader/build/libs/ \n"
+  curl -OL https://ci.lucko.me/job/LuckPerms/lastSuccessfulBuild/artifact/bukkit/loader/build/libs/LuckPerms-Bukkit-5.3.48.jar
   # floodgate-bukkit Geyser-Spigot https://geysermc.org/
   wget https://ci.opencollab.dev/job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/spigot/target/Geyser-Spigot.jar
   wget https://ci.opencollab.dev/job/GeyserMC/job/Floodgate/job/master/lastSuccessfulBuild/artifact/spigot/target/floodgate-spigot.jar
@@ -82,24 +85,32 @@ cd ${BASEDIR}/server && cp ${BASEDIR}/tools/buildtools/spigot*.jar ./spigot.jar
   # Multiverse: Core, Inventries, SignPortals, Portals https://www.curseforge.com/minecraft/bukkit-plugins/multiverse-core
   MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD}Multiverse: \n"
   MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD} - Core https://www.curseforge.com/minecraft/bukkit-plugins/multiverse-core \n"
+  curl -OL https://media.forgecdn.net/files/3362/854/Multiverse-Core-4.3.0.jar
   MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD} - Inventories https://www.curseforge.com/minecraft/bukkit-plugins/multiverse-inventories \n"
+  curl -OL https://media.forgecdn.net/files/3222/929/Multiverse-Inventories-4.2.2.jar
   MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD} - Portals https://www.curseforge.com/minecraft/bukkit-plugins/multiverse-portals \n"
+  curl -OL https://media.forgecdn.net/files/3113/114/Multiverse-Portals-4.2.1.jar
   MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD} - SignPortals https://www.curseforge.com/minecraft/bukkit-plugins/multiverse-signportals \n"
+  curl -OL https://media.forgecdn.net/files/3074/605/Multiverse-SignPortals-4.2.0.jar
   # StayPut: https://www.spigotmc.org/resources/stayput.34848/
   MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD}StayPut: https://www.spigotmc.org/resources/stayput.34848/ \n"
+  # TODO: Get spigot-Download working curl -L stayput.jar https://www.spigotmc.org/resources/stayput.34848/download?version=181463
   # Plot² Price: 15€ https://www.spigotmc.org/resources/plotsquared-v6.77506/
   MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD}Plot² (Payed!): \n"
   MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD} - Plugin https://www.spigotmc.org/resources/plotsquared-v6.77506 \n"
   MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD} - Dependency WorldEdit https://www.curseforge.com/minecraft/bukkit-plugins/worldedit \n"
+  curl -OL https://media.forgecdn.net/files/3283/695/worldedit-bukkit-7.2.5-dist.jar
   # ChestShop https://www.curseforge.com/minecraft/bukkit-plugins/chestshop
-  LATEST=$(get_latest_release ChestShop-authors/ChestShop-3)
-  curl -OL https://github.com/ChestShop-authors/ChestShop-3/releases/download/${LATEST}/ChestShop.jar
+  curl -OL https://ci.minebench.de/job/ChestShop-3/lastSuccessfulBuild/artifact/target/ChestShop.jar
   # MoreMobHeads2 https://www.curseforge.com/minecraft/bukkit-plugins/moremobheads2
   MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD}MoreMobHeads2: https://www.curseforge.com/minecraft/bukkit-plugins/moremobheads2 \n"
+  curl -OL https://media.forgecdn.net/files/3364/46/MoreMobHeads-1.15_1.0.21.jar
   # Heart https://www.curseforge.com/minecraft/bukkit-plugins/heart
   MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD}Heart: https://www.curseforge.com/minecraft/bukkit-plugins/heart \n"
+  curl -OL https://media.forgecdn.net/files/3316/502/Heart-RELEASE-1.0.jar
   # ImageOnMap https://www.curseforge.com/minecraft/bukkit-plugins/imageonmap
   LATEST=$(get_latest_release zDevelopers/ImageOnMap)
   curl -OL https://github.com/zDevelopers/ImageOnMap/releases/download/${LATEST}/ImageOnMap-${LATEST[@]/v/}.jar
+
 
   printf "${MANUAL_DOWNLOAD}"
--- update.sh
+++ update.sh
@@ -31,6 +31,7 @@ cd ${BASEDIR}/server/plugins/backup && rm *.jar
 mv ${BASEDIR}/server/plugins/*.jar ${BASEDIR}/server/plugins/backup
 cd ${BASEDIR}/server/plugins/
 
+MANUAL_DOWNLOAD="\nPlease Download these Plugins manually! \n\n"
 # BackupOnEvent https://www.curseforge.com/minecraft/bukkit-plugins/backuponevent
 LATEST=$(get_latest_release enayet123/BackupOnEventPlugin)
 curl -OL https://github.com/enayet123/BackupOnEventPlugin/releases/download/${LATEST}/BackupOnEvent.jar
@@ -40,13 +41,46 @@ curl -OL https://github.com/MilkBowl/Vault/releases/download/${LATEST}/Vault.jar
 # Essentials https://www.curseforge.com/minecraft/bukkit-plugins/essentialsx
 LATEST=$(get_latest_release EssentialsX/Essentials)
 curl -OL https://github.com/EssentialsX/Essentials/releases/download/${LATEST}/EssentialsX-${LATEST}.0.jar
-
+curl -OL https://github.com/EssentialsX/Essentials/releases/download/${LATEST}/EssentialsXChat-${LATEST}.0.jar
+# GriefPrevention https://www.curseforge.com/minecraft/bukkit-plugins/grief-prevention
+MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD}GriefPrevention: https://www.curseforge.com/minecraft/bukkit-plugins/grief-prevention \n"
+# ArmorStandEditor https://www.curseforge.com/minecraft/bukkit-plugins/armor-stand-edit
+MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD}ArmorStandEditor: https://www.curseforge.com/minecraft/bukkit-plugins/armor-stand-edit \n"
+# LuckPerms https://github.com/lucko/LuckPerms
+MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD}LuckPerms: https://ci.lucko.me/job/LuckPerms/lastSuccessfulBuild/artifact/bukkit/loader/build/libs/ \n"
 # floodgate-bukkit Geyser-Spigot https://geysermc.org/
 wget https://ci.opencollab.dev/job/GeyserMC/job/Geyser/job/master/lastSuccessfulBuild/artifact/bootstrap/spigot/target/Geyser-Spigot.jar
 wget https://ci.opencollab.dev/job/GeyserMC/job/Floodgate/job/master/lastSuccessfulBuild/artifact/spigot/target/floodgate-spigot.jar
+LATEST=$(get_latest_release Camotoy/GeyserSkinManager)
+curl -OL https://github.com/Camotoy/GeyserSkinManager/releases/download/${LATEST}/GeyserSkinManager-Spigot.jar
+# emotecraft https://www.curseforge.com/minecraft/bukkit-plugins/emotecraft-bukkit
+LATEST=$(get_latest_release KosmX/emotes)
+curl -OL https://github.com/KosmX/emotes/releases/download/${LATEST}/emotecraft-${LATEST:0:5}-bukkit.jar
+# Multiverse: Core, Inventries, SignPortals, Portals https://www.curseforge.com/minecraft/bukkit-plugins/multiverse-core
+MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD}Multiverse: \n"
+MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD} - Core https://www.curseforge.com/minecraft/bukkit-plugins/multiverse-core \n"
+MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD} - Inventories https://www.curseforge.com/minecraft/bukkit-plugins/multiverse-inventories \n"
+MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD} - Portals https://www.curseforge.com/minecraft/bukkit-plugins/multiverse-portals \n"
+MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD} - SignPortals https://www.curseforge.com/minecraft/bukkit-plugins/multiverse-signportals \n"
+# StayPut: https://www.spigotmc.org/resources/stayput.34848/
+MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD}StayPut: https://www.spigotmc.org/resources/stayput.34848/ \n"
+# Plot² Price: 15€ https://www.spigotmc.org/resources/plotsquared-v6.77506/
+MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD}Plot² (Payed!): \n"
+MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD} - Plugin https://www.spigotmc.org/resources/plotsquared-v6.77506 \n"
+MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD} - Dependency WorldEdit https://www.curseforge.com/minecraft/bukkit-plugins/worldedit \n"
+# ChestShop https://www.curseforge.com/minecraft/bukkit-plugins/chestshop
+LATEST=$(get_latest_release ChestShop-authors/ChestShop-3)
+curl -OL https://github.com/ChestShop-authors/ChestShop-3/releases/download/${LATEST}/ChestShop.jar
+# MoreMobHeads2 https://www.curseforge.com/minecraft/bukkit-plugins/moremobheads2
+MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD}MoreMobHeads2: https://www.curseforge.com/minecraft/bukkit-plugins/moremobheads2 \n"
+# Heart https://www.curseforge.com/minecraft/bukkit-plugins/heart
+MANUAL_DOWNLOAD="${MANUAL_DOWNLOAD}Heart: https://www.curseforge.com/minecraft/bukkit-plugins/heart \n"
+# ImageOnMap https://www.curseforge.com/minecraft/bukkit-plugins/imageonmap
+LATEST=$(get_latest_release zDevelopers/ImageOnMap)
+curl -OL https://github.com/zDevelopers/ImageOnMap/releases/download/${LATEST}/ImageOnMap-${LATEST[@]/v/}.jar
 
-echo "Please Download the following Plugins manually: \
-GriefPrevention https://www.curseforge.com/minecraft/bukkit-plugins/grief-prevention \
-ArmorStandEditor https://www.curseforge.com/minecraft/bukkit-plugins/armor-stand-edit \
-emotecraft https://github.com/KosmX/emotes/releases/latest \
-LuckPerms https://luckperms.net/ "
+printf "${MANUAL_DOWNLOAD}"
+
+
+
+read -p "Press Enter to start thes Server" </dev/tty
